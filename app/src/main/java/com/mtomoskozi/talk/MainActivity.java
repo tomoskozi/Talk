0432fb7cb77f1b63e60d42b6dd0227e3f93f0cb1
Tomoskozi Mate <tomsokozi.mate@gmail.com>
2016-04-07 15:23:35 +0200

    call is working now

--- app/src/main/java/com/mtomoskozi/talk/MainActivity.java
+++ app/src/main/java/com/mtomoskozi/talk/MainActivity.java
@@ -29,13 +29,9 @@ import java.util.Enumeration;
 
 public class MainActivity extends Activity {
 
+    private static final String TAG = "Talk";
     AudioGroup m_AudioGroup;
     AudioStream m_AudioStream;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,18 +45,19 @@ public class MainActivity extends Activity {
             audio.setMode(AudioManager.MODE_IN_COMMUNICATION);
             m_AudioGroup = new AudioGroup();
             m_AudioGroup.setMode(AudioGroup.MODE_NORMAL);
+            String localIP = ((EditText) findViewById(R.id.editText2)).getText().toString();
+            ((EditText) findViewById(R.id.editText2)).setText(InetAddress.getByAddress(getLocalIPAddress()).getHostAddress());
             m_AudioStream = new AudioStream(InetAddress.getByAddress(getLocalIPAddress()));
             int localPort = m_AudioStream.getLocalPort();
             m_AudioStream.setCodec(AudioCodec.PCMU);
             m_AudioStream.setMode(RtpStream.MODE_NORMAL);
 
             ((TextView) findViewById(R.id.lblLocalPort)).setText(String.valueOf(localPort));
-
-            ((Button) findViewById(R.id.button1)).setOnClickListener(new View.OnClickListener() {
+            findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
 
                 @Override
                 public void onClick(View v) {
-                    String remoteAddress = ((EditText) findViewById(R.id.editText2)).getText().toString();
+                    String remoteAddress = ((EditText) findViewById(R.id.editText3)).getText().toString();
                     String remotePort = ((EditText) findViewById(R.id.editText1)).getText().toString();
 
                     try {
@@ -76,7 +73,7 @@ public class MainActivity extends Activity {
                 }
             });
 
-            ((Button) findViewById(R.id.button2)).setOnClickListener(new View.OnClickListener() {
+            findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
 
                 @Override
                 public void onClick(View v) {
@@ -85,22 +82,19 @@ public class MainActivity extends Activity {
             });
 
         } catch (Exception e) {
-            Log.e("----------------------", e.toString());
+            Log.e(TAG, e.toString());
             e.printStackTrace();
         }
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    public static byte[] getLocalIPAddress() {
-        byte ip[] = null;
+    public static byte[] getLocalIPAddress () {
+        byte ip[]=null;
         try {
-            for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements(); ) {
+            for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
                 NetworkInterface intf = en.nextElement();
-                for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements(); ) {
+                for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
                     InetAddress inetAddress = enumIpAddr.nextElement();
-                    if (!inetAddress.isLoopbackAddress()) {
+                    if (!inetAddress.isLoopbackAddress() && inetAddress.getAddress().length == 4) {
                         ip = inetAddress.getAddress();
                     }
                 }
@@ -110,45 +104,5 @@ public class MainActivity extends Activity {
         }
         return ip;
 
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://com.mtomoskozi.talk/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://com.mtomoskozi.talk/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
     }
 }
